3b7a5a7dd113fa89fe253df313f7bb78cecf1cd8
Evgeny Galitskiy <gha665@gmail.com>
2020-02-14 23:10:25 -0500

    updated dragon-routes

--- routes/dragon-routes/dragon.js
+++ routes/dragon-routes/dragon.js
@@ -1,30 +1,30 @@
-const express = require('express');
+  
+const express = require("express");
 const router = express.Router();
-const Dragon = require('../../models/Dragon')
+const Dragon = require("../../models/Dragon");
+const uploadCloud = require("../../config/cloudinary-setup");
 
-/* GET Dragon home page */
-router.get('/', (req, res, next) => {
-  res.render('dragons/dragons-home');
+/* Dragon Home Page */
+router.get("/", (req, res, next) => {
+    Dragon.find()
+        .then(allDragonsFromDB => {
+            res.render("dragons/dragons-home", { dragons: allDragonsFromDB });
+        })
+        .catch(err => next(err));
 });
-
 
 // our create route for the new dragon
-router.post('/create', (req, res, next) => {
-  console.log(req.body);
+router.post("/create", uploadCloud.single("image"), (req, res, next) => {
+    console.log({ body: req.body, file: req.file });
+    const dragonInputInfo = req.body;
+    dragonInputInfo.image = req.file.url;
+    Dragon.create(dragonInputInfo)
+        .then(newlyCreatedDragon => {
+            console.log({ newlyCreatedDragon });
 
-  const dragonInputInfo = req.body;
-  // dragonInputInfo.image = req.file.url;
-
-  Dragon.create(dragonInputInfo)
-    .then(newlyCreatedDragon => {
-      console.log({
-        newlyCreatedDragon
-      });
-
-      res.redirect('back');
-    })
-    .catch(err => next(err));
+            res.redirect("back");
+        })
+        .catch(err => next(err));
 });
-
 
 module.exports = router;
